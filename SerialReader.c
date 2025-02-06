3fbd1dd4b7ee7e27aba4e4ea131fc7975e5da161
drbrodix <adamrigely@pm.me>
2025-02-06 12:40:43 +0100

    Frame checking functions are complete (even if not optimised), basic serial connection will be built.

--- SerialReader.c
+++ SerialReader.c
@@ -1,20 +1,25 @@
 #include "SerialReader.h"
 
-#define INPUT_ARRAY_LENGTH 200
-#define FT12_ARRAY_LENGTH 250
-#define FT12_START_BYTE 0x68
-#define FT12_END_BYTE 0x16
-#define FIND_FT12_END_BYTE(START_BYTE_INDEX, PAYLOAD_LENGTH) (START_BYTE_INDEX + 4 + PAYLOAD_LENGTH)
+#include <string.h>
 
-int lookForFTStart(const char* pBuff, int buffLen)
+void printBuff(const unsigned char* pBuff, int buffLen)
+{
+    for (int i = 0; i < buffLen; i++)
+    {
+        printf("%x, ", pBuff[i]);
+    }
+    printf("\n");
+}
+
+int lookForFT12Start(const unsigned char* pBuff, int buffLen, int* patternCheckingStartIndex)
 {
     int startByteIndex = -1;
 
-    for (int i = 0; i < buffLen; i++)
+    for (; *patternCheckingStartIndex < buffLen; (*patternCheckingStartIndex)++)
     {
-        if (pBuff[i] == FT12_START_BYTE)
+        if (pBuff[*patternCheckingStartIndex] == FT12_START_BYTE)
         {
-            startByteIndex = i;
+            startByteIndex = *patternCheckingStartIndex;
             break;
         }
     }
@@ -22,66 +27,145 @@ int lookForFTStart(const char* pBuff, int buffLen)
     return startByteIndex;
 }
 
-bool checkPattern(const char* pBuff, int startByteIndex)
+int checkPattern(const unsigned char* pBuff, int startByteIndex)
 {
     bool isHeaderOk     = false;
     bool isEndByteFound = false;
 
-    isHeaderOk =((pBuff[startByteIndex]       == pBuff[startByteIndex + 3]) &&
-                (pBuff[startByteIndex + 1]   == pBuff[startByteIndex + 2])
+                // Check matching start bytes
+    isHeaderOk =((pBuff[startByteIndex]     == pBuff[startByteIndex + 3]) &&
+                // Check matching length bytes
+                (pBuff[startByteIndex + 1]  == pBuff[startByteIndex + 2])
                 );
 
-    isEndByteFound = pBuff[FIND_FT12_END_BYTE(startByteIndex, pBuff[startByteIndex + 1])];
+    const int END_BYTE_INDEX = FIND_FT12_END_BYTE(startByteIndex, pBuff[startByteIndex + 1]);
+    isEndByteFound = pBuff[END_BYTE_INDEX] == FT12_END_BYTE;
 
-    return isHeaderOk && isEndByteFound;
+    return ((isHeaderOk && isEndByteFound) ? END_BYTE_INDEX : -1);
 }
 
-int findFt12Frame(const char* pBuff, int buffLen, int* destBuff)
+bool checkChecksum(const unsigned char* pBuff, int startByteIndex, int endByteIndex)
 {
-    const int startByteIndex = lookForFTStart(pBuff, buffLen);
+    const int CONTROL_BYTE_INDEX    = startByteIndex + 4;
+    const int CHECKSUM_BYTE_INDEX   = endByteIndex - 1;
+    unsigned int sum = 0;
 
-    if(startByteIndex < 0)
-        return -1;
-    else
+    for (int i = CONTROL_BYTE_INDEX; i < CHECKSUM_BYTE_INDEX; i++)
     {
-        return checkPattern(pBuff, startByteIndex);
+        sum += pBuff[i];
     }
+    return ((sum % 256) == pBuff[CHECKSUM_BYTE_INDEX]);
 }
 
-void printBuff(const char* pBuff, int buffLen)
+STATES readBuffer(const unsigned char* pBuff, const int buffLen, unsigned char* destBuff)
 {
-    for (int i = 0; i < buffLen; i++)
+    STATES currentState = CHECKING;
+    int startByteIndex = -1;
+    int endByteStartIndex = -1;
+    int patternCheckingStartIndex = 0;
+
+    while (patternCheckingStartIndex < buffLen)
     {
-        printf("%x, ", pBuff[i]);
+        startByteIndex = lookForFT12Start(pBuff, buffLen, &patternCheckingStartIndex);
+
+        if(startByteIndex < 0)
+        {
+            currentState = START_BYTE_NOT_FOUND;
+            return currentState;
+        }
+
+        endByteStartIndex = checkPattern(pBuff, startByteIndex);
+
+        if(endByteStartIndex >= 0)
+            break;
+
+        patternCheckingStartIndex++;
     }
+
+    if(endByteStartIndex < 0)
+    {
+        currentState = PATTERN_MISMATCH;
+        return currentState;
+    }
+
+    if(!checkChecksum(pBuff, startByteIndex, endByteStartIndex))
+    {
+        currentState = CHECKSUM_ERROR;
+        return currentState;
+    }
+    currentState = FRAME_OK;
+    memcpy_s(destBuff, FT12_ARRAY_LENGTH, pBuff + startByteIndex, GET_FT12_LENGTH(startByteIndex, endByteStartIndex));
+    return currentState;
 }
 
 int main(int argc, char const *argv[])
 {
     // Test input buffer array
-    const int testPack[INPUT_ARRAY_LENGTH]={0x00, 0x68, 0x0c, 0x0c,
-                                            0x68, 0x73, 0xf0, 0x06,
-                                            0x00, 0x03, 0x00, 0x01,
-                                            0x00, 0x03, 0x03, 0x01,
-                                            0x01, 0x75, 0x16, 0x00};
+    /*const unsigned char testPack[INPUT_ARRAY_LENGTH] =   {0x68, 0x68, 0x16, 0x68,
+                                                            0x68, 0x68, 0x0c, 0x0c,
+                                                            0x68, 0x73, 0xf0, 0x06,
+                                                            0x00, 0x03, 0x00, 0x01,
+                                                            0x00, 0x03, 0x03, 0x01,
+                                                            0x01, 0x75, 0x16, 0x0e};*/
 
     // Buffer array for storing incoming FT 1.2 frame
-    const int ft12Buffer[FT12_ARRAY_LENGTH] = {};
+    unsigned char ft12Buffer[FT12_ARRAY_LENGTH] = {};
+    unsigned char buffer[INPUT_ARRAY_LENGTH] = {};
+    HANDLE serialHandle;
+    COMMTIMEOUTS timeouts;
+    DWORD read;
+    DCB port;
+    const char commName[128] = "\\\\.\\COM3";
 
-    // printBuff(ft12Buffer, FT12_ARRAY_LENGTH);
-    
-    printf("%d", findFt12Frame(testPack, INPUT_ARRAY_LENGTH, ft12Buffer));
-    // const LPCWSTR COM_NAME = "COM3";
+    // open the comm port.
+    serialHandle = CreateFile(commName,
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
 
-    // HANDLE serialHandle = CreateFile(
-    //                                     COM_NAME,
-    //                                     GENERIC_READ | GENERIC_WRITE,
-    //                                     0,
-    //                                     NULL,
-    //                                     OPEN_EXISTING,
-    //                                     FILE_ATTRIBUTE_NORMAL,
-    //                                     NULL
-    //                                 );
+    if(INVALID_HANDLE_VALUE == serialHandle) {
+        printf("Couldn't open port");
+        return 1;
+    }
+
+    // get the current DCB, and adjust a few bits to our liking.
+    memset(&port, 0, sizeof(port));
+    port.DCBlength = sizeof(port);
+    if(!GetCommState(serialHandle, &port))
+        printf("Couldn't get comm state");
+
+    port.BaudRate = CBR_19200;
+    port.ByteSize = 8;
+    port.StopBits = ONESTOPBIT;
+    port.Parity   = EVENPARITY;
+    if(!SetCommState(serialHandle, &port))
+        printf("Couldn't adjust port settings");
+
+    // set short timeouts on the comm port.
+    timeouts.ReadIntervalTimeout = 1;
+    timeouts.ReadTotalTimeoutMultiplier = 1;
+    timeouts.ReadTotalTimeoutConstant = 1;
+    timeouts.WriteTotalTimeoutMultiplier = 1;
+    timeouts.WriteTotalTimeoutConstant = 1;
+    if(!SetCommTimeouts(serialHandle, &timeouts))
+        printf("Couldn't set port timeouts.");
+
+    while(true)
+    {
+        // check for data on port and display it on screen.
+        ReadFile(serialHandle, buffer, sizeof(buffer), &read, NULL);
+        if(read)
+        {
+            printf("%d\n", readBuffer(buffer, sizeof(buffer), ft12Buffer));
+
+            printBuff(ft12Buffer, FT12_ARRAY_LENGTH);
+
+            /*printBuff(buffer, sizeof(buffer));*/
+        }
+    }
 
     return 0;
 }
--- SerialReader.h
+++ SerialReader.h
@@ -2,9 +2,25 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-// #include <Windows.h>
+#include <Windows.h>
 
-void printBuff(const char* pBuff, int buffLen);
-int lookForFTStart(const char* pBuff, int buffLen);
-bool checkPattern(const char* pBuff, int startByteIndex);
-int findFt12Frame(const char* pBuff, int buffLen, int* destBuff);
+#define INPUT_ARRAY_LENGTH 200
+#define FT12_ARRAY_LENGTH 250
+#define FT12_START_BYTE 0x68
+#define FT12_END_BYTE 0x16
+#define FIND_FT12_END_BYTE(START_BYTE_INDEX, PAYLOAD_LENGTH) (START_BYTE_INDEX + 5 + PAYLOAD_LENGTH)
+#define GET_FT12_LENGTH(START_BYTE_INDEX, END_BYTE_INDEX) ((END_BYTE_INDEX - START_BYTE_INDEX) + 1)
+
+typedef enum {
+    CHECKING                = 0,
+    FRAME_OK                = 1,
+    START_BYTE_NOT_FOUND    = 2,
+    PATTERN_MISMATCH        = 3,
+    CHECKSUM_ERROR          = 4,
+} STATES;
+
+STATES readBuffer(const unsigned char* pBuff, const int buffLen, unsigned char* destBuff);
+void printBuff(const unsigned char* pBuff, int buffLen);
+int lookForFT12Start(const unsigned char* pBuff, int buffLen, int* patternCheckingStartIndex);
+int checkPattern(const unsigned char* pBuff, int startByteIndex);
+bool checkChecksum(const unsigned char* pBuff, int startByteIndex, int endByteIndex);
