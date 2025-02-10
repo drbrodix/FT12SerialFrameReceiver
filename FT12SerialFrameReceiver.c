b710f4ac4d9673c5e533e5a3ee42eb0780156f4b
drbrodix <adamrigely@pm.me>
2025-02-10 12:58:05 +0100

    App is now sort of usable. A basic state machine for the FT 1.2 frame has been implemented. It is now handled if more than one frame is in the buffer.

--- FT12SerialFrameReceiver.c
+++ FT12SerialFrameReceiver.c
@@ -8,49 +8,6 @@ void printBuff(const unsigned char* pBuff, DWORD buffLen) {
     printf("\n");
 }
 
-DWORD lookForFT12Start(const unsigned char* pBuff, DWORD buffLen, DWORD* patternCheckingStartIndex) {
-    DWORD startByteIndex = -1;
-
-    for (; *patternCheckingStartIndex < buffLen; (*patternCheckingStartIndex)++)
-    {
-        if (pBuff[*patternCheckingStartIndex] == FT12_START_BYTE)
-        {
-            startByteIndex = *patternCheckingStartIndex;
-            break;
-        }
-    }
-    
-    return startByteIndex;
-}
-
-DWORD checkPattern(const unsigned char* pBuff, DWORD startByteIndex) {
-    bool isHeaderOk     = false;
-    bool isEndByteFound = false;
-
-                // Check matching start bytes
-    isHeaderOk =((pBuff[startByteIndex]     == pBuff[startByteIndex + 3]) &&
-                // Check matching length bytes
-                (pBuff[startByteIndex + 1]  == pBuff[startByteIndex + 2])
-                );
-
-    const DWORD END_BYTE_INDEX = FIND_FT12_END_BYTE(startByteIndex, pBuff[startByteIndex + 1]);
-    isEndByteFound = pBuff[END_BYTE_INDEX] == FT12_END_BYTE;
-
-    return ((isHeaderOk && isEndByteFound) ? END_BYTE_INDEX : -1);
-}
-
-bool checkChecksum(const unsigned char* pBuff, DWORD startByteIndex, DWORD endByteIndex) {
-    const DWORD CONTROL_BYTE_INDEX    = startByteIndex + 4;
-    const DWORD CHECKSUM_BYTE_INDEX   = endByteIndex - 1;
-    unsigned int sum = 0;
-
-    for (DWORD i = CONTROL_BYTE_INDEX; i < CHECKSUM_BYTE_INDEX; i++)
-    {
-        sum += pBuff[i];
-    }
-    return ((sum % 256) == pBuff[CHECKSUM_BYTE_INDEX]);
-}
-
 STATES readBuffer(const unsigned char* pBuff, const DWORD bytesRead, unsigned char* destBuff, ReaderInfo* ri) {
 
     // typedef enum {
@@ -59,9 +16,9 @@ STATES readBuffer(const unsigned char* pBuff, const DWORD bytesRead, unsigned ch
     //     CHECKING_SECOND_LENGTH      = 2,
     //     CHECKING_SECOND_START_BYTE  = 3,
     //     CHECKING_CONTROL_BYTE       = 4,
-    //     HEADER_FOUND                = 5,
-    //     CHECKSUM_ERROR              = 6,
-    //     FRAME_OK                    = 7
+    //     CHECKING_BAOS_PAYLOAD       = 5,
+    //     CHECKING_CHECKSUM           = 6,
+    //     CHECKING_END_BYTE           = 7,
     // } STATES;
     //
     // typedef struct {
@@ -69,66 +26,99 @@ STATES readBuffer(const unsigned char* pBuff, const DWORD bytesRead, unsigned ch
     //     DWORD currentInputIndex;
     //     DWORD currentOutputIndex;
     //     unsigned char payloadLength;
+    //     int checksumSum;
+    //     bool doStartBytesMatch;
+    //     bool doLengthBytesMatch;
+    //     bool doesChecksumMatch;
+    //     bool isEndByteFound;
     // } ReaderInfo;
 
     while (ri->currentInputIndex < bytesRead) {
         switch (ri->currentState) {
 
             case SEARCHING_START_BYTE:
-                for (; ri->currentInputIndex < bytesRead; ri->currentInputIndex++) {
+                while (ri->currentInputIndex < bytesRead) {
                     if (pBuff[ri->currentInputIndex] == FT12_START_BYTE) {
                         destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
                         ri->currentState = CHECKING_FIRST_LENGTH;
+                        ri->currentInputIndex++;
+                        ri->currentOutputIndex++;
                         break;
                     }
+                    ri->currentInputIndex++;
+                    ri->currentOutputIndex++;
                 }
             break;
 
             case CHECKING_FIRST_LENGTH:
                 destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->payloadLength = pBuff[ri->currentInputIndex];
                 ri->currentState = CHECKING_SECOND_LENGTH;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+            break;
+
+            case CHECKING_SECOND_LENGTH:
+                destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->doLengthBytesMatch = destBuff[ri->currentOutputIndex] == destBuff[ri->currentOutputIndex - 1];
+                ri->currentState = CHECKING_SECOND_START_BYTE;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+            break;
+
+            case CHECKING_SECOND_START_BYTE:
+                destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->doStartBytesMatch = destBuff[ri->currentOutputIndex] == destBuff[ri->currentOutputIndex - 3];
+                ri->currentState = CHECKING_CONTROL_BYTE;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+            break;
+
+            case CHECKING_CONTROL_BYTE:
+                destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->checksumSum = destBuff[ri->currentOutputIndex];
+                ri->currentState = CHECKING_BAOS_PAYLOAD;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+            break;
+
+            case CHECKING_BAOS_PAYLOAD:
+                // It must be checked, that we don't try to access empty elements in the array,
+                // and it is also necessary to ensure that we only access the payload part of the frame.
+                while  ((ri->currentInputIndex < bytesRead) &&
+                        (ri->currentOutputIndex <= GET_PAYLOAD_END_INDEX(ri->payloadLength))) {
+
+                    destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                    ri->checksumSum += destBuff[ri->currentOutputIndex];
+                    ri->currentInputIndex++;
+                    ri->currentOutputIndex++;
+                }
+                ri->currentState = CHECKING_CHECKSUM;
+            break;
+
+            case CHECKING_CHECKSUM:
+                destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->doesChecksumMatch = (ri->checksumSum % 256) == destBuff[ri->currentOutputIndex];
+                ri->currentState = CHECKING_END_BYTE;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+            break;
+
+            case CHECKING_END_BYTE:
+                destBuff[ri->currentOutputIndex] = pBuff[ri->currentInputIndex];
+                ri->isEndByteFound = (destBuff[ri->currentOutputIndex] == FT12_END_BYTE);
+                ri->currentState = RECEPTION_COMPLETE;
+                ri->currentInputIndex++;
+                ri->currentOutputIndex++;
+                return ri->currentState;
+            break;
 
             default:
                 break;
         }
     }
 
-    return currentState;
-
-    /*int startByteIndex = -1;
-    int endByteStartIndex = -1;
-    int patternCheckingStartIndex = 0;
-
-    while (patternCheckingStartIndex < buffLen) {
-        startByteIndex = lookForFT12Start(pBuff, buffLen, &patternCheckingStartIndex);
-
-        if(startByteIndex < 0) {
-            return currentState;
-        }
-
-        endByteStartIndex = checkPattern(pBuff, startByteIndex);
-
-        if(endByteStartIndex >= 0)
-            break;
-
-        patternCheckingStartIndex++;
-    }
-
-    if(endByteStartIndex < 0) {
-        return currentState;
-    }
-
-    if(!checkChecksum(pBuff, startByteIndex, endByteStartIndex)) {
-        currentState = CHECKSUM_ERROR;
-        return currentState;
-    }
-    currentState = FRAME_OK;
-    memcpy_s(destBuff,
-        FT12_ARRAY_LENGTH,
-        pBuff + startByteIndex,
-        GET_FT12_LENGTH(startByteIndex, endByteStartIndex));
-
-    return currentState;*/
+    return ri->currentState;
 }
 
 // Thread function to handle user input.
@@ -147,19 +137,6 @@ int main(int argc, char const *argv[]) {
                                                             0x00, 0x03, 0x03, 0x01,
                                                             0x01, 0x75, 0x16, 0x0e};*/
 
-    /*while(true) {
-        // check for data on port and display it on screen.
-        ReadFile(serialHandle, buffer, sizeof(buffer), &dwRead, NULL);
-        if(dwRead) {
-            printf("%d\n", readBuffer(buffer, sizeof(buffer), ft12Buffer));
-
-            printBuff(ft12Buffer, FT12_ARRAY_LENGTH);
-
-            /*printBuff(buffer, sizeof(buffer));#1#
-        }
-    }*/
-
-    // Open the serial port in overlapped mode.
     // Open the serial port in overlapped mode.
     HANDLE hSerial = CreateFile(
         "COM3",                        // Change this as needed (e.g., "COM3")
@@ -229,11 +206,7 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    ReaderInfo ri{
-    SEARCHING_START_BYTE,
-    0,
-    0,
-    0};
+    ReaderInfo ri = {0};
 
     printf("Press [Enter] to stop listening...\n");
 
@@ -270,9 +243,37 @@ int main(int argc, char const *argv[]) {
             }
         }
 
+        // If actual data has been received
         if (bytesRead > 0) {
-            readBuffer(buffer, bytesRead, ft12Frame, &ri);
-            printBuff(ft12Frame, sizeof(ft12Frame));
+            // While the buffer has not been completely read
+            while (ri.currentInputIndex < bytesRead) {
+                readBuffer(buffer, bytesRead, ft12Frame, &ri);
+
+                printBuff(ft12Frame, sizeof(ft12Frame));
+                printf("Start byte match: %d\n"
+                       "Length byte match: %d\n"
+                       "Checksum match: %d\n"
+                       "End byte found: %d\n"
+                       "Data left in input buffer... Data: %d\n",
+                                ri.doStartBytesMatch,
+                                ri.doLengthBytesMatch,
+                                ri.doesChecksumMatch,
+                                ri.isEndByteFound,
+                                bytesRead - ri.currentInputIndex);
+
+                // If the frame has been completely read
+                if (ri.currentState == RECEPTION_COMPLETE) {
+                    ri.currentState = SEARCHING_START_BYTE;
+                    const DWORD currentInputIndex = ri.currentInputIndex;
+                    memset(&ft12Frame, 0, sizeof(ft12Frame));
+                    memset(&ri, 0, sizeof(ri));
+                    ri.currentInputIndex = currentInputIndex;
+                }
+            }
+            // Reset buffer and reader struct if buffer has been completely read
+                memset(&ri, 0, sizeof(ri));
+                memset(&ft12Frame, 0, sizeof(ft12Frame));
+                memset(&buffer, 0, sizeof(buffer));
         }
         // Reset the event for the next read.
         ResetEvent(osRead.hEvent);
--- FT12SerialFrameReceiver.h
+++ FT12SerialFrameReceiver.h
@@ -12,6 +12,7 @@
 #define FT12_END_BYTE       0x16
 #define FIND_FT12_END_BYTE(START_BYTE_INDEX, PAYLOAD_LENGTH) (START_BYTE_INDEX + 5 + PAYLOAD_LENGTH)
 #define GET_FT12_LENGTH(START_BYTE_INDEX, END_BYTE_INDEX) ((END_BYTE_INDEX - START_BYTE_INDEX) + 1)
+#define GET_PAYLOAD_END_INDEX(PAYLOAD_LENGTH) (PAYLOAD_LENGTH + 3)
 
 typedef enum {
     SEARCHING_START_BYTE        = 0,
@@ -19,9 +20,10 @@ typedef enum {
     CHECKING_SECOND_LENGTH      = 2,
     CHECKING_SECOND_START_BYTE  = 3,
     CHECKING_CONTROL_BYTE       = 4,
-    HEADER_FOUND                = 5,
-    CHECKSUM_ERROR              = 6,
-    FRAME_OK                    = 7
+    CHECKING_BAOS_PAYLOAD       = 5,
+    CHECKING_CHECKSUM           = 6,
+    CHECKING_END_BYTE           = 7,
+    RECEPTION_COMPLETE          = 8,
 } STATES;
 
 typedef struct {
@@ -29,11 +31,13 @@ typedef struct {
     DWORD currentInputIndex;
     DWORD currentOutputIndex;
     unsigned char payloadLength;
+    int checksumSum;
+    bool doStartBytesMatch;
+    bool doLengthBytesMatch;
+    bool doesChecksumMatch;
+    bool isEndByteFound;
 } ReaderInfo;
 
 STATES readBuffer(const unsigned char* pBuff, const DWORD bytesRead, unsigned char* destBuff, ReaderInfo* ri);
 void printBuff(const unsigned char* pBuff, DWORD buffLen);
-DWORD lookForFT12Start(const unsigned char* pBuff, DWORD buffLen, DWORD* patternCheckingStartIndex);
-DWORD checkPattern(const unsigned char* pBuff, DWORD startByteIndex);
-bool checkChecksum(const unsigned char* pBuff, DWORD startByteIndex, DWORD endByteIndex);
 DWORD WINAPI InputThread(LPVOID lpParameter);
